3c903ea0e658dd7033f179e035ed7c7b2aaca18a
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2022-03-19 00:55:07 +0200

    Renaming

--- main.py
+++ main.py
@@ -25,15 +25,15 @@ class MainWindow(QMainWindow):
         self.signal_to_delete=None
         # Links of GUI Elements to Methods:
         #! Update 
-        self.ui.pushButton.setCheckable(True)
+        self.ui.showHidePushButton.setCheckable(True)
         self.ui.actionOpen.triggered.connect(lambda: self.openFile())
-        self.ui.pushButton.clicked.connect(lambda: self.showHideGraph())
-        self.ui.plot_button.clicked.connect(self.signal_composer)
-        self.ui.add_button.clicked.connect(self.signal_summation)
-        self.ui.delete_button.clicked.connect(self.signal_deletion)
-        self.ui.comboBox.activated.connect(self.select_signal)
+        self.ui.showHidePushButton.clicked.connect(lambda: self.showHideGraph())
+        self.ui.plotPushButton.clicked.connect(self.signal_composer)
+        self.ui.addPushButton.clicked.connect(self.signal_summation)
+        self.ui.deletePushButton.clicked.connect(self.signal_deletion)
+        self.ui.deleteSignalComboBox.activated.connect(self.select_signal)
         #! Make it ratio from fmax
-        self.ui.horizontalSlider.valueChanged['int'].connect(self.ui.lcdNumber.display)
+        self.ui.samplingHorizontalSlider.valueChanged['int'].connect(self.ui.frequencyLcdNumber.display)
     # Methods
 
     # def open(self):
@@ -47,23 +47,23 @@ class MainWindow(QMainWindow):
         self.TimeReadings = self.data_frame.iloc[:,0].to_numpy()
         self.AmplitudeReadings = self.data_frame.iloc[:,1].to_numpy()
         #! Update 
-        self.ui.graphicsView.clear()
-        self.ui.graphicsView.plot(self.TimeReadings, self.AmplitudeReadings, pen=pyqtgraph.mkPen('r', width=1.5))
+        self.ui.mainGraphicsView.clear()
+        self.ui.mainGraphicsView.plot(self.TimeReadings, self.AmplitudeReadings, pen=pyqtgraph.mkPen('r', width=1.5))
         # self.ui.graphicsView.plot(self.TimeReadings, self.AmplitudeReadings, pen=pyqtgraph.mkPen('b', width=1.5), symbol='o', symbolPen ='b', symbolBrush = 0.9)
     
     def showHideGraph(self):
-        if self.ui.pushButton.isChecked():
-            self.ui.graphicsView_2.hide()
+        if self.ui.showHidePushButton.isChecked():
+            self.ui.reconstrucedGraphicsView.hide()
         else:
-            self.ui.graphicsView_2.show()
+            self.ui.reconstrucedGraphicsView.show()
 
     def signal_composer(self):
-        self.ui.graphicsView_3.clear()
-        self.frequency= float(self.ui.lineEdit.text())
-        self.amplitude= float(self.ui.lineEdit_2.text()) 
-        self.phase_shift= float(self.ui.lineEdit_3.text())
+        self.ui.composerGraphicsView.clear()
+        self.frequency= float(self.ui.frequencyLineEdit.text())
+        self.amplitude= float(self.ui.amplotudeLineEdit.text()) 
+        self.phase_shift= float(self.ui.phaseShiftLineEdit.text())
         self.signal = self.amplitude * np.sin(2 * np.pi * self.frequency * self.time + self.phase_shift)
-        self.ui.graphicsView_3.plot(self.time, self.signal, pen=pyqtgraph.mkPen('r', width=1.5))
+        self.ui.composerGraphicsView.plot(self.time, self.signal, pen=pyqtgraph.mkPen('r', width=1.5))
         
 
     def signal_summation(self):
@@ -71,26 +71,26 @@ class MainWindow(QMainWindow):
          #frequency stored in a list for later sampling
         # self.added_composer_signals_frequency.append(self.frequency)
         self.added_signals_list.append(self.signal)
-        self.ui.comboBox.addItem('F='+ str(self.frequency)+ 'A=' +str(self.amplitude) + 'PS='+ str(self.phase_shift))
-        self.ui.graphicsView_4.clear() #adjust it to the right graph
+        self.ui.deleteSignalComboBox.addItem('F='+ str(self.frequency)+ 'A=' +str(self.amplitude) + 'PS='+ str(self.phase_shift))
+        self.ui.summationGraphicsView.clear() #adjust it to the right graph
         # self.ui.graphicsView_4.plot(self.time, signal1+signal2, pen=pyqtgraph.mkPen('r', width=1.5))
-        self.ui.graphicsView_4.plot(self.time, self.added_composer_signals, pen=pyqtgraph.mkPen('r', width=1.5))
+        self.ui.summationGraphicsView.plot(self.time, self.added_composer_signals, pen=pyqtgraph.mkPen('r', width=1.5))
        
         #get maximum frequency for sampling
         # maximum_frequency=np.max(self.added_composer_signals_frequency)
 
     def select_signal(self):
-        self.signal_index=self.ui.comboBox.currentIndex()
+        self.signal_index=self.ui.deleteSignalComboBox.currentIndex()
     def signal_deletion(self):
-        self.ui.graphicsView_4.clear()
+        self.ui.summationGraphicsView.clear()
         self.signal_to_delete=self.added_signals_list.pop(self.signal_index)
         # self.added_composer_signals_frequency.pop(self.signal_index)
-        self.ui.comboBox.removeItem(self.signal_index)
-        if self.ui.comboBox.count()==0:
-            self.ui.graphicsView_4.clear()
+        self.ui.deleteSignalComboBox.removeItem(self.signal_index)
+        if self.ui.deleteSignalComboBox.count()==0:
+            self.ui.summationGraphicsView.clear()
         else:
             self.added_composer_signals-=self.signal_to_delete
-            self.ui.graphicsView_4.plot(self.time, self.added_composer_signals, pen=pyqtgraph.mkPen('r', width=1.5))
+            self.ui.summationGraphicsView.plot(self.time, self.added_composer_signals, pen=pyqtgraph.mkPen('r', width=1.5))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
