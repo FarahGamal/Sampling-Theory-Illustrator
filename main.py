deb88abc29b4b0f0294d9f7d0c8be71138d100f5
RoaaEhab <73796828+RoaaEhab@users.noreply.github.com>
2022-03-18 23:09:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pyqtgraph import PlotWidget
 import pandas as pd
 from GUI import Ui_MainWindow
 import csv
+import numpy as np
 
 class MainWindow(QMainWindow):
 
@@ -16,12 +17,21 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         # Variables Initialization
-    
+        self.time = np.linspace(-2.5, 2.5, 1000)
+        self.added_composer_signals=0
+        self.added_signals_list=[]
+        # self.added_composer_signals_frequency=[]
+        self.signal_index=None
+        self.signal_to_delete=None
         # Links of GUI Elements to Methods:
         #! Update 
         self.ui.pushButton.setCheckable(True)
         self.ui.actionOpen.triggered.connect(lambda: self.openFile())
         self.ui.pushButton.clicked.connect(lambda: self.showHideGraph())
+        self.ui.plot_button.clicked.connect(self.signal_composer)
+        self.ui.add_button.clicked.connect(self.signal_summation)
+        self.ui.delete_button.clicked.connect(self.signal_deletion)
+        self.ui.comboBox.activated.connect(self.select_signal)
         #! Make it ratio from fmax
         self.ui.horizontalSlider.valueChanged['int'].connect(self.ui.lcdNumber.display)
     # Methods
@@ -37,13 +47,50 @@ class MainWindow(QMainWindow):
         self.TimeReadings = self.data_frame.iloc[:,0].to_numpy()
         self.AmplitudeReadings = self.data_frame.iloc[:,1].to_numpy()
         #! Update 
-        self.ui.graphicsView.plot(self.TimeReadings, self.AmplitudeReadings, pen=pyqtgraph.mkPen('b', width=1.5), symbol='o', symbolPen ='b', symbolBrush = 0.9)
+        self.ui.graphicsView.clear()
+        self.ui.graphicsView.plot(self.TimeReadings, self.AmplitudeReadings, pen=pyqtgraph.mkPen('r', width=1.5))
+        # self.ui.graphicsView.plot(self.TimeReadings, self.AmplitudeReadings, pen=pyqtgraph.mkPen('b', width=1.5), symbol='o', symbolPen ='b', symbolBrush = 0.9)
     
     def showHideGraph(self):
         if self.ui.pushButton.isChecked():
             self.ui.graphicsView_2.hide()
         else:
             self.ui.graphicsView_2.show()
+
+    def signal_composer(self):
+        self.ui.graphicsView_3.clear()
+        self.frequency= float(self.ui.lineEdit.text())
+        self.amplitude= float(self.ui.lineEdit_2.text()) 
+        self.phase_shift= float(self.ui.lineEdit_3.text())
+        self.signal = self.amplitude * np.sin(2 * np.pi * self.frequency * self.time + self.phase_shift)
+        self.ui.graphicsView_3.plot(self.time, self.signal, pen=pyqtgraph.mkPen('r', width=1.5))
+        
+
+    def signal_summation(self):
+        self.added_composer_signals+=self.signal
+         #frequency stored in a list for later sampling
+        # self.added_composer_signals_frequency.append(self.frequency)
+        self.added_signals_list.append(self.signal)
+        self.ui.comboBox.addItem('F='+ str(self.frequency)+ 'A=' +str(self.amplitude) + 'PS='+ str(self.phase_shift))
+        self.ui.graphicsView_4.clear() #adjust it to the right graph
+        # self.ui.graphicsView_4.plot(self.time, signal1+signal2, pen=pyqtgraph.mkPen('r', width=1.5))
+        self.ui.graphicsView_4.plot(self.time, self.added_composer_signals, pen=pyqtgraph.mkPen('r', width=1.5))
+       
+        #get maximum frequency for sampling
+        # maximum_frequency=np.max(self.added_composer_signals_frequency)
+
+    def select_signal(self):
+        self.signal_index=self.ui.comboBox.currentIndex()
+    def signal_deletion(self):
+        self.ui.graphicsView_4.clear()
+        self.signal_to_delete=self.added_signals_list.pop(self.signal_index)
+        # self.added_composer_signals_frequency.pop(self.signal_index)
+        self.ui.comboBox.removeItem(self.signal_index)
+        if self.ui.comboBox.count()==0:
+            self.ui.graphicsView_4.clear()
+        else:
+            self.added_composer_signals-=self.signal_to_delete
+            self.ui.graphicsView_4.plot(self.time, self.added_composer_signals, pen=pyqtgraph.mkPen('r', width=1.5))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
